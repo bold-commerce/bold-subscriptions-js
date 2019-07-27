900d1eee2f726eed2c00e444f651659e1a27d46e
Morgan Wowk <morgan@boldcommerce.com>
2019-07-26 22:44:08 -0500

    Implement getSlots and getGroupInfo

--- src/BoldApi/subscriptionGroups/index.js
+++ src/BoldApi/subscriptionGroups/index.js
@@ -1,3 +1,17 @@
-export function getGroupInfo(groupId, myshopifyDomain = false) {
-    // TODO: Implement getGroupInfo()
+import { getShopifyDomain } from '../../helpers';
+
+/**
+ * @param groupId
+ */
+export function getGroupInfo(groupId) {
+    fetch(
+        `https://ro.boldapps.net/api/widget/group/${groupId}?shop=${getShopifyDomain()}&ts=${Date.now()}`,
+        {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+        }
+    )
+        .then(data => data.json());
 }
