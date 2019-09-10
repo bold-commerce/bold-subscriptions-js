f0cd47872966a9a41e528b10c0be84d76f884f46
Morgan Wowk <morgan@boldcommerce.com>
2019-09-09 22:00:23 -0500

    Minor v1.2.0
    * Support for Recurring Cart Mode of Bold Subscriptions
    * Added recurringCartCheckout method to checkout object

--- src/BoldCheckout/index.js
+++ src/BoldCheckout/index.js
@@ -1,6 +1,54 @@
 import { getShopifyDomain, getPrimaryDomain, getShopifyHandleFromDomain } from '../helpers';
 import { default as I } from '../Validator';
 import { removeProductDescriptionsFromCart } from '../helpers';
+import { RECURRING_CART_CHECKOUT_EXPECTED_OPTIONS } from '../constants';
+
+function recurringCartCheckout(e) {
+    e.preventDefault();
+
+    let result = I.expectClickEventWithinForm('recurringCartCheckout', e);
+    if (result instanceof Error) {
+        throw result;
+    }
+
+    const target = e.target;
+    const form = target.form;
+    const formData = new FormData(form);
+
+    result = I.expectFormData('recurringCartCheckout', formData, RECURRING_CART_CHECKOUT_EXPECTED_OPTIONS);
+    if (result instanceof Error) {
+        throw result;
+    }
+
+    fetch(
+        `https://${getPrimaryDomain()}/cart.json?ts=${Date.now()}`,
+        {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+        }
+    )
+        .then(cartObj => cartObj.json())
+        .then((cartJSON) => {
+            removeProductDescriptionsFromCart(cartJSON);
+
+            const cartStr = JSON.stringify(cartJSON);
+            const myshopifyDomain = getShopifyDomain();
+
+            const cartEl = document.createElement('INPUT');
+            cartEl.name = 'shopify_cart';
+            cartEl.value = cartStr;
+            cartEl.type = 'hidden';
+
+            form.action = `https://recurringcheckout.com/s/${getShopifyHandleFromDomain(myshopifyDomain)}/checkout/recurring_full_cart?shop_url=${myshopifyDomain}`;
+            form.method = 'POST';
+            form.enctype = 'multipart/form-data';
+
+            form.appendChild(cartEl);
+            form.submit();
+        });
+}
 
 function goToCheckout(e) {
     let result = I.expectClickEventWithinForm('goToCheckout', e);
@@ -30,6 +78,7 @@ function goToCheckout(e) {
             const cartEl = document.createElement('INPUT');
             cartEl.name = 'shopify_cart';
             cartEl.value = cartStr;
+            cartEl.type = 'hidden';
 
             form.action = `https://recurringcheckout.com/s/${getShopifyHandleFromDomain(myshopifyDomain)}/checkout/recurring?shop_url=${myshopifyDomain}`;
             form.method = 'POST';
@@ -87,4 +136,5 @@ function goToCheckoutCashier(e) {
 export {
     goToCheckout,
     goToCheckoutCashier,
+    recurringCartCheckout,
 };
